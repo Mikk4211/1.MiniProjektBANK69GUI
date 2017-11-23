61f0bd07faa251c7d22710b329125d065ac9b45a
wexr <wexr.kk@gmail.com>
2017-11-23 14:10:22 +0100

    Ændret DB til BankOf69Projekt

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,7 +1,41 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class Controller {
 
+    @FXML
+    private Button btn_nyKunde;
+
+    @FXML
+    private TextField input_nyFornavn;
+    @FXML
+    private TextField input_nyEfternavn;
+    @FXML
+    private TextField input_nyCprFirst;
+    @FXML
+    private TextField input_nyCprLast;
+    @FXML
+    private TextField input_nyAdr;
+    @FXML
+    private Label lbl;
+    int tempCprFirst, tempCprLast;
+    String tempFornavn = null, tempEfternavn, tempAdr;
+    char symbol;
+
+    @FXML
+    private void handleButtonAction(ActionEvent e){
+        int num1, num2, answer=0;
+        char symbol = '+';
+        //e tells what button was clicked
+        if(e.getSource()==btn_nyKunde){
+            toString(input_nyFornavn.getText(tempFornavn));
+            tempCprFirst=Integer.parseInt(input_nyCprFirst.getText());
+            tempCprLast=Integer.parseInt(input_nyCprLast.getText());
 
 
 
