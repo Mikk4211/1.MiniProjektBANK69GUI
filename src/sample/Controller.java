273bca15e986101a1bfa7a52e22ddedc50cca2f1
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-11-25 17:09:40 +0100

    har gjort så knapper og parse virker. I controller

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,11 +7,19 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.*;
 import javafx.scene.control.*;
+/*
+* I Sample.fxml, når man laver en ny button skal du hardcode onAction="#handleButtonAction" ind i, da intellij+scenebuilder er skrald.
+* - Mads
+* */
+
+
 
 public class Controller {
-DB_Statements sqlStatement = new DB_Statements();
+//DB_Statements sqlStatement = new DB_Statements();
 
 
+    @FXML
+    private Button btn_nyKonto;
     @FXML
     private Button btn_nyKunde;
     @FXML
@@ -26,24 +34,36 @@ DB_Statements sqlStatement = new DB_Statements();
     private TextField input_nyAdr;
     @FXML
     private Label lbl;
-    int tempCprFirst, tempCprLast;
+
+    public int tempCprFirst = 0, tempCprLast = 0;
     String tempFornavn, tempEfternavn, tempAdr;
     char symbol;
 
     @FXML
     private void handleButtonAction(ActionEvent e) {
-        int num1, num2, answer = 0;
-        char symbol = '+';
         //e tells what button was clicked
         if (e.getSource() == btn_nyKunde) {
-            // toString(input_nyFornavn.getText(tempFornavn));
+            //giver fornavn værdien:
             tempFornavn = input_nyFornavn.getText();
-            tempCprFirst = Integer.parseInt(input_nyCprFirst.getText());
-            tempCprLast = Integer.parseInt(input_nyCprLast.getText());
 
-            System.out.println(tempCprFirst + "-" + tempCprLast + "REEE!"+tempFornavn);
+            //Giver cprFirst en værdi:
+            /*
+            Man kan af en eller anden grund ikke gøre sådan:
+            //tempCprFirst = Integer.parseInt( input_nyCprFirst.getText());
+            //tempCprLast = Integer.parseInt(input_nyCprLast.getText());
+            man skal først hente den til en variable og derefter parse den.
+             */
+            String tempCprFirstParser = input_nyCprFirst.getText();
+            tempCprFirst = Integer.parseInt(tempCprFirstParser);
+
+
+
+            //System.out.println(tempCprFirst + "-" + tempCprLast + "REEE!"+tempFornavn);
+            System.out.println(tempCprFirst+tempFornavn);
             return;
         }
+
+
     }
 
 }
