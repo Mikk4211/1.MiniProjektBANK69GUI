286ffbecd4e562dfbab96602481193bed7a8c22b
wexr <wexr.kk@gmail.com>
2017-11-27 11:24:05 +0100

    drop table ved database opret.

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -94,6 +94,7 @@ DB_Statements sqlStatement = new DB_Statements();
         /*Knap der opretter db og table */
         if (e.getSource() == btn_opretDBTing) {
             sqlStatement.createDB();
+            sqlStatement.dropTable();
             sqlStatement.createKundeTable();
             sqlStatement.insertTestKunder();
         }
