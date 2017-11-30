704af13c1ac69bb6524a417867fea5150bd5f171
Mads Nielsen <31951027+TheCaptain420@users.noreply.github.com>
2017-11-30 09:39:14 +0100

    Javadocs til Main

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,6 +6,14 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main.
+ * Kører hele programmet/applikationen.
+ *
+ * @author Mathias Bruun(TejsFrank), Mads Nielsen(TheCaptain420), Johan Stenboeg(JohanStenboeg), Benjamin Ejrup(Wexr), Mikkel Sørensen(Mikk4211).
+ */
+
+
 public class Main extends Application {
 
     @Override
@@ -13,15 +21,19 @@ public class Main extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setResizable(false);
 
+        /**
+         * Opretter og viser "vinduet", ved hjælp af javafx methoder.
+         */
         primaryStage.setTitle("BANK OF SIXTY NINE");
         primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
-
+    /**
+     * @param args Commando linjens argumenter.
+     */
     public static void main(String[] args) {
         launch(args);
-
     }
 }
 
